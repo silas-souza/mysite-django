2a8e5ee17eb8f1bbb2b00033b83a779b5f7f21ce
silas souza <silas.souzameister1@gmail.com>
2026-04-04 19:52:10 -0300

    feat: adiciona templates base e index com configuração de diretório

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 from blog import views
-from blog.views.post_view import PostView
+from blog.views.post_view import PostView, PostDetailView
 
 urlpatterns = [
     path('', views.PostView.as_view(), name='home'),
+    path("<slug:slug>/", PostDetailView.as_view(), name='post_detail'),
 ]
